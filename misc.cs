985703a938e746c34142d9b6cfb15c093c58eab6
BenTang1939 <45980590+BenTang1939@users.noreply.github.com>
2020-03-08 16:57:56 +0000

    Update misc.cs

--- misc.cs
+++ misc.cs
@@ -253,6 +253,152 @@ namespace DiscordBot.Modules{
             public async Task CleanSongDirectory()
             {
                 await m_Service.RemoveDuplicateSongsAsync();
+            }
+
+            FunService funService = new FunService();
+
+            [Name("SS"), Summary("Screenshare")]
+            [Command("SS", RunMode = RunMode.Async), RequireContext(ContextType.Guild), Alias("s")]
+            public async Task Screenshare([Remainder]SocketVoiceChannel channel = null)
+                => await ReplyAsync(embed: funService.GetScreenshareEmbed(Context.Guild.CurrentUser, channel, Context.Guild));
+
+            [Name("Avatar"), Summary("Returns the user's pfp")]
+            [Command("ava", RunMode = RunMode.Async), Alias("a")]
+            public async Task Avatar([Remainder]SocketUser user = null)
+                => await ReplyAsync(embed: funService.GetAvatarEmbed(Context.Message, user));
+
+            [Name("ID"), Summary("Returns an user ID")]
+            [Command("RandomID", RunMode = RunMode.Async), Alias("rID")]
+            public async Task RandomUser()
+                => await ReplyAsync($"<@{funService.GetRandomUserId(Context.Client)}>");
+            
+            }
+
+         [Name ("User Info")]
+        [Command ("userinfo")]
+        [Summary ("Gets information about the specified user")]
+        [CommandOptions (typeof (UserInfoOptions))]
+        public async Task UserInfo ([Remainder] IGuildUser user)
+        {
+            var options = GetOptions<UserInfoOptions> ();
+
+            EmbedBuilder emb = new EmbedBuilder();
+
+            string userRoles = DiscordHelpers.GetListOfUsersRoles (user);
+            IRole highestRole = DiscordHelpers.GetUsersHigherstRole (user);
+            if (highestRole != null)
+                emb.Color = highestRole.Color;
+            EmbedAuthorBuilder author = new EmbedAuthorBuilder ();
+            author.Name = user.Username;
+            if (user.IsBot)
+                author.Name += " (Bot)";
+            else if (user.IsWebhook)
+                author.Name += " (Webhook)";
+
+            emb.Author = author;
+
+            if (string.IsNullOrEmpty (user.AvatarId))
+                emb.ThumbnailUrl = $"https://discordapp.com/assets/dd4dbc0016779df1378e7812eabaa04d.png";
+            else
+                emb.ThumbnailUrl = $"https://cdn.discordapp.com/avatars/{user.Id}/{user.AvatarId}.png";
+
+            EmbedFooterBuilder footer = new EmbedFooterBuilder();
+            footer.Text = $"User info requested by {Context.User.Username}";
+            if (string.IsNullOrEmpty (Context.User.AvatarId))
+                footer.IconUrl = $"https://discordapp.com/assets/dd4dbc0016779df1378e7812eabaa04d.png";
+            else
+                footer.IconUrl = $"https://cdn.discordapp.com/avatars/{Context.User.Id}/{Context.User.AvatarId}.png";
+            emb.Footer = footer;
+
+            emb.Description = $"User information for {user.Username}#{user.Discriminator} | {user.Id}";
+
+            emb.AddField("Created account at", user.CreatedAt.ToString("dd MMM yyyy, HH:mm"));
+
+            emb.AddField("Joined server at", ((DateTimeOffset)user.JoinedAt).ToString("dd MMM yyyy, HH:mm"));
+
+            if (string.IsNullOrEmpty(userRoles) == false)
+                emb.AddField("Role(s)", userRoles);
+
+            // Display the list of all of user's permissions
+            string userPermissions = GetUserPermissions (user);
+
+            if (string.IsNullOrEmpty (userPermissions) == false)
+                emb.AddField ("Permissions", userPermissions);
+
+            emb.AddField("Online status", user.Status == UserStatus.DoNotDisturb ? "Do Not Disturb" : user.Status.ToString());
+
+            if (user.Game.HasValue)
+            {
+                if (user.Game.Value.StreamType == StreamType.Twitch)
+                {
+                    emb.AddField("Streaming", user.Game.Value.StreamUrl);
+                }
+                else
+                {
+                    emb.AddField("Playing", user.Game.Value.Name);
+                }
+            }
+
+            await SendMessage (options, "", false, emb.Build());
+        }
+
+            private string GetUserPermissions (IGuildUser user)
+            {
+                string permissions = "";
+
+                if (Context.Guild.OwnerId == user.Id)
+                {
+                    permissions += "Owner";
+                    return permissions;
+                }
+
+                if (user.GuildPermissions.Administrator)
+                {
+                    permissions += "Administrator";
+                    return permissions;
+                }
+
+                if (user.GuildPermissions.BanMembers)
+                    permissions += "Ban Memebers, ";
+            
+                if (user.GuildPermissions.DeafenMembers)
+                    permissions += "Deafen Members, ";
+
+                if (user.GuildPermissions.KickMembers)
+                    permissions += "Kick Members, ";
+
+                if (user.GuildPermissions.ManageChannels)
+                    permissions += "Manage Channels, ";
+
+                if (user.GuildPermissions.ManageEmojis)
+                    permissions += "Manage Emojis, ";
+
+                if (user.GuildPermissions.ManageGuild)
+                    permissions += "Manage Guild, ";
+
+                if (user.GuildPermissions.ManageMessages)
+                    permissions += "Manage Messages, ";
+
+                if (user.GuildPermissions.ManageNicknames)
+                    permissions += "Manage Nicknames, ";
+
+                if (user.GuildPermissions.ManageRoles)
+                    permissions += "Manage Roles, ";
+
+                if (user.GuildPermissions.ManageWebhooks)
+                    permissions += "Manage Webhooks, ";
+
+                if (user.GuildPermissions.MentionEveryone)
+                    permissions += "Mention Everyone, ";
+
+                if (user.GuildPermissions.MoveMembers)
+                    permissions += "Move Members, ";
+
+                if (user.GuildPermissions.MuteMembers)
+                    permissions += "Mute Members, ";
+
+                return permissions.Remove (permissions.Length - 2);
+        }
             
     }
 }
